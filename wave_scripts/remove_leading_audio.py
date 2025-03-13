0d30261c01d40b4e3469550644b78788f8e016ff
kennydoit <ken.r.moore@gmail.com>
2025-03-13 13:23:04 -0400

    added comments

--- wave_scripts/remove_leading_audio.py
+++ wave_scripts/remove_leading_audio.py
@@ -1,18 +1,26 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 
 
 from pydub import AudioSegment
 from pydub.silence import detect_nonsilent
 import logging
+import os
 
 logging.basicConfig(level=logging.INFO)
 
 def remove_leading_silence(input_file, output_file, silence_thresh=-50, min_silence_len=1000):
+    """
+    Removes leading silence from an audio file.
+
+    Parameters:
+    input_file (str): Path to the input audio file.
+    output_file (str): Path to save the output audio file.
+    silence_thresh (int): Silence threshold in dB. Default is -50 dB.
+    min_silence_len (int): Minimum length of silence in milliseconds. Default is 1000 ms.
+
+    Returns:
+    None
+    """
     audio = AudioSegment.from_wav(input_file)
 
     # Detect non-silent parts (returns a list of tuples representing non-silent ranges)
@@ -35,31 +43,27 @@ def remove_leading_silence(input_file, output_file, silence_thresh=-50, min_sile
         logging.info("No non-silent parts found in the audio. No output file created.")
         return None
 
-#%%
-
-remove_leading_silence("D://Audio//KCJ_Am_128_Vocal_Phrase_Loop_GiveMeSomething_Wet.wav", 
-                       "D://Audio//KCJ_Am_128_Vocal_Phrase_Loop_GiveMeSomething_Wet.wav",
-                       -40, 
-                       250)
-
-
-#%%
-
-import os
-import logging
-
-logging.basicConfig(level=logging.INFO)
-
 def process_folder(folder_path, silence_thresh=-50, min_silence_len=250):
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".wav"):
-            input_file = os.path.join(folder_path, filename)
-            remove_leading_silence(input_file, input_file, silence_thresh, min_silence_len)
+    """
+    Processes all .wav files in a given folder and its sub-folders to remove leading silence.
+
+    Parameters:
+    folder_path (str): Path to the folder containing .wav files.
+    silence_thresh (int): Silence threshold in dB. Default is -50 dB.
+    min_silence_len (int): Minimum length of silence in milliseconds. Default is 250 ms.
+
+    Returns:
+    None
+    """
+    for root, _, files in os.walk(folder_path):
+        for filename in files:
+            if filename.endswith(".wav"):
+                input_file = os.path.join(root, filename)
+                logging.info(f"Processing file: {input_file}")
+                remove_leading_silence(input_file, input_file, silence_thresh, min_silence_len)
 
 # Example usage
 folder_path = "C://Program Files//Image-Line//Packs//Vocals//Black Octopus Sound - Katty Heath Vocal Sample Pack"
-
-
 process_folder(folder_path, -50, 150)
 
 
